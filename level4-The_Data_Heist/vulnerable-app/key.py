4d4e6da87589164b312ddc1674ab32948a686a69
RamCharan <ramcharanvunnam@gmail.com>
2025-11-06 17:20:37 +0530

    Added secret.key to the repo (cause charan's been whining too much)

--- level4-The_Data_Heist/vulnerable-app/key.py
+++ level4-The_Data_Heist/vulnerable-app/key.py
@@ -1,4 +1,4 @@
-with open("secret.key", "rb") as key_file:
+with open("C:/Users/ramch/OneDrive/Desktop/HacktoberFest2025_CyberSec/level4-The_Data_Heist/vulnerable-app/secret.key", "rb") as key_file:
     key=key_file.read()
 
 
